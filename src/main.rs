49b3404e97cb365207ea9ee27391a29fa2a3fc49
Titus Abele <tvmab@pm.me>
2024-12-13 10:59:03 +0100

    added compression

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use std::path::PathBuf;
 fn main() {
     let matches = Command::new("Wasmadder")
         .version("0.1.0")
-        .about("CLI tool to append data to a custom section in a WebAssembly binary")
+        .about("CLI tool to add data to a custom section in a WebAssembly binary")
         .subcommand(
-            Command::new("append")
-                .about("Append data to a custom section in a WebAssembly binary")
+            Command::new("add")
+                .about("Add data to a custom section in a WebAssembly binary")
                 .arg(
                     Arg::new("input")
                         .short('i')
@@ -20,7 +20,7 @@ fn main() {
                     Arg::new("data")
                         .short('d')
                         .long("data")
-                        .help("Data file to be appended")
+                        .help("Data file to be added")
                         .required(true)
                         .value_parser(clap::value_parser!(PathBuf)),
                 )
@@ -42,27 +42,35 @@ fn main() {
         )
         .get_matches();
 
-    if let Some(sub_matches) = matches.subcommand_matches("append") {
+    if let Some(sub_matches) = matches.subcommand_matches("add") {
         let input_file = sub_matches.get_one::<PathBuf>("input").unwrap();
         let data_file = sub_matches.get_one::<PathBuf>("data").unwrap();
         let section_name = sub_matches.get_one::<String>("section").unwrap();
         let output_file = sub_matches.get_one::<PathBuf>("output").unwrap();
 
-        handle_append(input_file, data_file, section_name, output_file);
+        handle_add(input_file, data_file, section_name, output_file);
     } else {
         eprintln!("No subcommand was used");
     }
 }
 
-fn handle_append(
+fn handle_add(
     input: &PathBuf,
     data: &PathBuf,
     section: &str,
     output: &PathBuf,
 ) {
-    let mut wasm_bytes = std::fs::read(input).expect("Failed to read input file");
+    let wasm_bytes = std::fs::read(input).expect("Failed to read input file");
     let data_bytes = std::fs::read(data).expect("Failed to read data file");
+    
+    // Start with the minimum valid WASM file
+    let mut result: Vec<u8> = vec![0, b'a', b's', b'm', 1, 0, 0, 0];
+    let compressed_data = miniz_oxide::deflate::compress_to_vec_zlib(&data_bytes, 7);
 
-    wasm_gen::write_custom_section(&mut wasm_bytes, section, &data_bytes);
+    wasm_gen::write_custom_section(&mut result, section, &compressed_data);
+
+    // Append the rest of the original WASM file
+    result.extend_from_slice(&wasm_bytes[8..]);
+
     std::fs::write(output, wasm_bytes).expect("Failed to write output file");
 }
